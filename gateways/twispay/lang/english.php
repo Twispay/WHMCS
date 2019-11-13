cd64f59e41544f45a81435837e484bb431fb45d0
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-11-13 16:02:41 +0200

    Added language files.

---
+++ gateways/twispay/lang/english.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Twispay english messages files.
+ *
+ * @package     Twispay_Payment_Gateway
+ * @author      Twispay
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+$_LANG['TWISPAY_CONFIGURATION_ERROR'] = "Payment failed: Incomplete or missing configuration.";
+
+$_LANG['TWISPAY_GENERAL_ERROR_TITLE']          = "An error has occured:";
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_F']         = 'The payment could not be processed. Please';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_TRY_AGAIN'] = ' try again';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_OR']        = ' or';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_CONTACT']   = ' contact';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_S']         = ' the website administrator.';
---
+++ gateways/twispay/lang/romanian.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Twispay romanian messages files.
+ *
+ * @package     Twispay_Payment_Gateway
+ * @author      Twispay
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+$_LANG['TWISPAY_CONFIGURATION_ERROR'] = "Plata esuata: Configuratie lipsa sau incompleta.";
+
+$_LANG['TWISPAY_GENERAL_ERROR_TITLE']          = "A aparut o eroare:";
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_F']         = 'Plata nu a putut fi procesata. Va rugam';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_TRY_AGAIN'] = ' incercati din nou';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_OR']        = ' sau';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_CONTACT']   = ' contactati';
+$_LANG['TWISPAY_GENERAL_ERROR_DESC_S']         = ' administratorul site-ului.';
